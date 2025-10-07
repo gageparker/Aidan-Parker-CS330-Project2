65f78764d3f98bb4f44f9b099dfcbe82e323cd64
gageparker <gageeparker@gmail.com>
2025-10-06 20:44:10 -0500

    updates

--- Fall2025-Project2-agparker9/Fall2025-Project2-agparker9/obj/Debug/net9.0/Fall2025-Project2-agparker9.AssemblyInfo.cs
+++ Fall2025-Project2-agparker9/Fall2025-Project2-agparker9/obj/Debug/net9.0/Fall2025-Project2-agparker9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fall2025-Project2-agparker9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f03035414e0d31552fcba7cc5e4c436840effd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c94034cc861b96f6c5b60debfc6556364356dd9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fall2025-Project2-agparker9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fall2025-Project2-agparker9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
