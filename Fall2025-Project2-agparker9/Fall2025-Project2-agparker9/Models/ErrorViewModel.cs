0f03035414e0d31552fcba7cc5e4c436840effd6
gageparker <gageeparker@gmail.com>
2025-10-06 19:42:15 -0500

    Add Project

---
+++ Fall2025-Project2-agparker9/Fall2025-Project2-agparker9/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fall2025_Project2_agparker9.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
